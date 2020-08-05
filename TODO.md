dde198432a8491dc4d0f8f51bf0895317e9915de
Nate Hofmann <inatejh@gmail.com>
2020-08-04 23:30:26 -0400

    mana cost formatter, basic text formatting

--- TODO.md
+++ TODO.md
@@ -1,7 +1,9 @@
 ### Parsing
 - lowercase all words
 - replace card names from text and flavor text with ~
-- remove commas, dashes, periods, semi-colons, colons
+- remove dashes, periods, semi-colons, colons
+- keep colons (period?)
+- split mana costs / tap functions 
 - split on spaces, newlines
 - remove apostrophes 
 - skip multi faced cards
--- gather_cards.py
+++ gather_cards.py
@@ -1,20 +1,46 @@
 import json as j
 from typing import List, Dict
+import re
+
+REMINDER_TEXT_REGEX = re.compile('\\(.+\\)')
+COST_REGEX = re.compile('{[0-9RGBWUC]+}')
+SPLIT_REGEX = re.compile('[, .\n-]')
+MANA_SYMBOLS_TO_IDX = {symbol: idx for idx, symbol in enumerate('WUBRGC')}
 
 
 def remove_card_name(card_name: str, text: str) -> str:
     return text.replace(card_name, '~')
 
 
-def format_mana_cost(mana_cost: str) -> List[str]:
-    return []
+def format_string(string: str) -> List[str]:
+    # remove reminder text
+    string = REMINDER_TEXT_REGEX.sub('', string)  # TODO finish me
+    print(string)
+    # split
+    words = SPLIT_REGEX.split(string)
+    # split colons
+    return [i.lower() for i in words if i]
+
+
+def format_mana_cost(mana_cost: str) -> List[int]:
+    # format as 7 item list - W U B R G colorless wastes
+    results = COST_REGEX.findall(mana_cost)
+    counts = [0 for i in range(7)]
+    for result in results:
+        result = result[1:-1]
+        try:
+            counts[MANA_SYMBOLS_TO_IDX[result]] += 1
+        except KeyError:
+            counts[-1] += int(result)
+    return counts
 
 
 def parse_card(card) -> dict:
     name = card['name']
-    mana_cost = card.get('mana_cost', '')
-    text = card.get('oracle_text', '')
-    flavor_text = card.get('flavor_text', '')
+    mana_cost = format_mana_cost(card.get('mana_cost', ''))
+    text = remove_card_name(name, card.get('oracle_text', ''))
+    flavor_text = remove_card_name(name, card.get('flavor_text', ''))
+    print(format_string(text))
     return {'name': name, 'mana_cost': mana_cost, 'text': text, 'flavor_text': flavor_text}
 
 
@@ -22,7 +48,6 @@ def parse_all_cards(json_path: str) -> None:
     with open(json_path, 'r') as f:
         raw_cards = j.load(f)
     parsed_cards = [parse_card(card) for card in raw_cards]
-    print(parsed_cards)
 
 
 if __name__ == '__main__':
